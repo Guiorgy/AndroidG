ff4854f9035770bcf10c5b595dd7cdab56b380e7
Guiorgy <Guiorgy123@gmail.com>
2019-06-26 22:23:52 +0400

    include javadoc

--- androidg/src/main/java/guiorgy/javag/util/List.java
+++ androidg/src/main/java/guiorgy/javag/util/List.java
@@ -1,6 +1,33 @@
 package guiorgy.javag.util;
 
 public interface List<E> extends java.util.List<E> {
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to 0.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param byteArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default byte[] toArray(byte[] byteArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
@@ -25,6 +52,33 @@ public interface List<E> extends java.util.List<E> {
         return byteArray;
     }
 
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to 0.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param shortArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default short[] toArray(short[] shortArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
@@ -49,6 +103,33 @@ public interface List<E> extends java.util.List<E> {
         return shortArray;
     }
 
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to 0.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param intArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default int[] toArray(int[] intArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
@@ -73,6 +154,33 @@ public interface List<E> extends java.util.List<E> {
         return intArray;
     }
 
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to 0.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param longArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default long[] toArray(long[] longArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
@@ -97,6 +205,33 @@ public interface List<E> extends java.util.List<E> {
         return longArray;
     }
 
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to 0.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param floatArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default float[] toArray(float[] floatArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
@@ -121,6 +256,33 @@ public interface List<E> extends java.util.List<E> {
         return floatArray;
     }
 
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to 0.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param doubleArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default double[] toArray(double[] doubleArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
@@ -145,6 +307,33 @@ public interface List<E> extends java.util.List<E> {
         return doubleArray;
     }
 
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to false.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param booleanArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default boolean[] toArray(boolean[] booleanArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
@@ -169,6 +358,33 @@ public interface List<E> extends java.util.List<E> {
         return booleanArray;
     }
 
+    /**
+     * Returns an array containing primitive equivalent of all of the
+     * elements in this list in proper sequence (from first to last element);
+     * The runtime type of the returned array is that of the specified array.
+     * If the list fits in the specified array, it is returned therein.
+     * Otherwise, a new array is allocated with the runtime type of the
+     * specified array and the size of this list.
+     *
+     * <p>If the list fits in the specified array with room to spare (i.e.,
+     * the array has more elements than the list), the elements in the array
+     * following the end of the list are set to 0.
+     *
+     * <p>Like the {@link #toArray(Object[])} method, this method acts as
+     * bridge between array-based and collection-based APIs.  Further, this
+     * method allows the return of primitive type arrays.
+     *
+     * @param charArray The array into which the equivalent primitives of the
+     *                  elements of this list are to be stored, if it is big
+     *                  enough; otherwise, a new array of the same runtime
+     *                  type is allocated for this purpose.
+     * @return An array containing the equivalent primitives the elements
+     * of this list
+     * @throws ArrayStoreException  If the runtime type of the specified array
+     *                              is not an equivalent primitive of the
+     *                              runtime type of every element in this list
+     * @throws NullPointerException If the specified array is null
+     */
     default char[] toArray(char[] charArray) throws ArrayStoreException, NullPointerException {
         final int size = size();
         if (size == 0) {
